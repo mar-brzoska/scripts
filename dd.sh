2cb4d6c9e159327406852aa5ab605ce213bc0e9d
mar-brzoska <mar-brzoska@users.noreply.github.com>
2014-10-18 23:31:24 +0200

    utworzony
    start

---
+++ dd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#----------------------------------------------------------
+# Skrypt instalacyjny CMS Drupal 7 dla Ubuntu 14 oraz Debian 7.
+# Skrypt tworzy vhosta Apache2, bazę danych oraz instaluje Drupala
+# wraz z dodatkowymi modułami i bibliotekami.
+# 
+#-----------------------------------------------------------------------
+
+DOMAINNAME=mark1.loc
+USER=marek
+WERSJA=7.31
+
